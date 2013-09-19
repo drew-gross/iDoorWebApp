bb921c9d687649fd5569345235f3e243db525804
Drew Gross <drew.a.gross@gmail.com>
2013-09-19 02:06:42 -0400

    send link

--- iDoor.rb
+++ iDoor.rb
@@ -23,11 +23,12 @@ get '/messages/:id' do
 end
 
 post '/messages' do
+	@message = Message.new(request.body.string)
 	RestClient.post API_URL+"/messages", 
 	    :from => "idoor@idoor.heroku.com",
 	    :to => "drew.a.gross@gmail.com",
 	    :subject => "New Message from iDoor!",
-	    :text => request.body.string
+	    :text => "http://www.herokuapp.com/messages/" + @message.id.to_s
 end
 
 message = <<MESSAGE_END
